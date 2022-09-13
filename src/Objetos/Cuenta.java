acec71f3bf6c5524f9e608ffd1c782af220a5b73
pascual tadeo rey <pascual-rey@hotmail.com>
2022-09-13 20:58:29 -0300

    agrega atributos

---
+++ src/Objetos/Cuenta.java
@@ -0,0 +1,9 @@
+package Objetos;
+
+public class Cuenta {
+    private float saldo;
+    private int numeroConsignaciones;
+    private int numeroDeRetiros;
+    private float tasaAnual;
+    private float comisionAnual;
+}
