1ed6ad8038e6464ec63a2297f251f9a22db76ce4
pascual tadeo rey <pascual-rey@hotmail.com>
2022-09-13 23:05:23 -0300

    agrega metodos

--- src/Objetos/CuentaDeAhorro.java
+++ src/Objetos/CuentaDeAhorro.java
@@ -3,4 +3,38 @@ package Objetos;
 public class CuentaDeAhorro extends Cuenta{
     private boolean estado;
 
+    public CuentaDeAhorro(float saldo, float tasaAnual, boolean estado) {
+        super(saldo, tasaAnual);
+        if (saldo < 10000)
+            this.estado = false;
+        else
+            this.estado = true;
+    }
+
+    public void depositarDinero(float monto){
+        if (estado)
+            super.depositarDinero(monto);
+    }
+
+    public void retirarDinero(float monto){
+        if (estado)
+            super.retirarDinero(monto);
+    }
+
+    public void extractoMensual() {
+        if (getNumeroDeRetiros() > 4){
+            float comisionMensual = getComisionMensual();
+            setComisionMensual(comisionMensual);
+        }
+        super.extractoMensual();
+        if (getSaldo() < 10000)
+            estado = false;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "CuentaDeAhorro{" +
+                "estado=" + estado +
+                '}';
+    }
 }
