68d5454e8094e738ff1ab2ca59002e2cfc2674a4
pascual tadeo rey <pascual-rey@hotmail.com>
2022-09-13 21:17:47 -0300

    agrega metodos

--- src/Objetos/Cuenta.java
+++ src/Objetos/Cuenta.java
@@ -5,5 +5,47 @@ public class Cuenta {
     private int numeroConsignaciones;
     private int numeroDeRetiros;
     private float tasaAnual;
-    private float comisionAnual;
+    private float comisionMensual;
+
+    public Cuenta(float saldo, float tasaAnual) {
+        numeroConsignaciones = 0;
+        numeroDeRetiros = 0;
+        this.saldo = saldo;
+        this.tasaAnual = tasaAnual;
+    }
+
+    public void depositarDinero(float monto){
+        saldo = saldo + monto;
+        numeroConsignaciones++;
+    }
+
+    public void retirarDinero(float monto){
+        if (monto <= saldo)
+            saldo = saldo - monto;
+        numeroDeRetiros++;
+    }
+
+    public void interesMensual(){
+        float tasaMensual = tasaAnual / 12;
+        float interesMensual = saldo * tasaMensual;
+        saldo = saldo + interesMensual;
+    }
+
+    public void estractoMensual(){
+        saldo = saldo - comisionMensual;
+        interesMensual();
+    }
+
+    @Override
+    public String toString() {
+        return "Cuenta{" +
+                "saldo=" + saldo +
+                ", numeroConsignaciones=" + numeroConsignaciones +
+                ", numeroDeRetiros=" + numeroDeRetiros +
+                ", tasaAnual=" + tasaAnual +
+                ", comisionMensual=" + comisionMensual +
+                '}';
+    }
 }
+
+
